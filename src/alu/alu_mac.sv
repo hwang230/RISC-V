72369073145b2fdedcbf31ee7b78b363bbf38e35
Hugh <hugh.wang@mail.mcgill.ca>
2026-04-16 13:30:59 -0400

    Updated MAC to take rd, rs1, rs2 as inputs

--- src/alu/alu_mac.sv
+++ src/alu/alu_mac.sv
@@ -2,9 +2,9 @@
 import cpu_pkg::*;
 
 module alu_mac(
-    input logic [31:0] a,
-    input logic [31:0] b, 
-    input logic [31:0] c, 
+    input logic [31:0] rs1_val,
+    input logic [31:0] rs2_val, 
+    input logic [31:0] rd_old_val, 
     input alu_op_e alu_op,
     output logic [31:0] result
 );
@@ -12,8 +12,8 @@ module alu_mac(
     always_comb begin
         case(alu_op)
             ALU_MAC: begin
-                temp = a*b;
-                result = temp[31:0] + c;
+                temp = rs1_val * rs2_val;
+                result = temp[31:0] + rd_old_val;
             end 
             default: result = '0;
         endcase
