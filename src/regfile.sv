35841ac83668428f7be9c0c400dfd911cc694e8a
Hugh <hugh.wang@mail.mcgill.ca>
2026-09-19 15:58:16 -0400

    Made regfile more granular to custom WIDTHS

--- src/regfile.sv
+++ src/regfile.sv
@@ -1,22 +1,26 @@
-module regfile(
+module regfile #(
+    parameter int DATA_WIDTH = 32,
+    parameter int ADDR_WIDTH = 5
+)(
     input logic clk,
     input logic we,
-    input logic [4:0] rs1_addr,
-    input logic [4:0] rs2_addr,
-    input logic [4:0] rd_addr, 
-    input logic [31:0] rd_data,
-    output logic [31:0] rs1_data,
-    output logic [31:0] rs2_data
+    input logic  [ADDR_WIDTH-1:0] rs1_addr,
+    input logic  [ADDR_WIDTH-1:0] rs2_addr,
+    input logic  [ADDR_WIDTH-1:0] rd_addr, 
+    input logic  [DATA_WIDTH-1:0] rd_data,
+    output logic [DATA_WIDTH-1:0] rs1_data,
+    output logic [DATA_WIDTH-1:0] rs2_data
 );
+    localparam int NUM_REGS = 1 << ADDR_WIDTH;
     // ignore x0 here
-    logic [31:0] regs [31:1]
-    assign rs1_data = (rs1_addr == 5'b0) ? 32'b0 : regs[rs1_addr];
-    assign rs2_data = (rs2_addr == 5'b0) ? 32'b0 : regs[rs2_addr];
+    logic [DATA_WIDTH-1:0] regs [1:NUM_REGS-1];
+    assign rs1_data = (rs1_addr == '0) ? '0 : regs[rs1_addr];
+    assign rs2_data = (rs2_addr == '0) ? '0 : regs[rs2_addr];
 
     always_ff @(posedge clk) begin
-        if (we && rd_addr != 5'b0) begin
+        if (we && rd_addr != '0) begin
             regs[rd_addr] <= rd_data;
         end
-
     end
+
 endmodule
